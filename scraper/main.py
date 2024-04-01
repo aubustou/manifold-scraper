2bd792590fd9db1f445376409e750b856a6f6119
aubustou <survivalfr@yahoo.fr>
2024-04-01 17:10:53 +0200

    add mocked

--- scraper/main.py
+++ scraper/main.py
@@ -1,12 +1,26 @@
+from __future__ import annotations
+
 import argparse
 import datetime
+import hashlib
+import logging
 from pathlib import Path
 
-from sqlalchemy import Column, DateTime, Integer, String, Text, create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy import Boolean, Column, DateTime, Integer, String, Text, create_engine
+from sqlalchemy.orm import Session, declarative_base, sessionmaker
 
 session: Session
+
+logger = logging.getLogger(__name__)
+
+
+class MockSession:
+    def add(self, model):
+        logger.info("Adding %s", model)
+
+    def commit(self):
+        logger.info("Committing")
+
 
 # Define the table model
 Base = declarative_base()
@@ -93,33 +107,112 @@ class Collection(Base):
     slug = Column(String)
 
 
+class ModelFile(Base):
+    """Table "public.model_files"
+             Column          |              Type              | Collation | Nullable |                 Default
+    -------------------------+--------------------------------+-----------+----------+-----------------------------------------
+     id                      | bigint                         |           | not null | nextval('model_files_id_seq'::regclass)
+     filename                | character varying              |           |          |
+     model_id                | bigint                         |           | not null |
+     created_at              | timestamp(6) without time zone |           | not null |
+     updated_at              | timestamp(6) without time zone |           | not null |
+     presupported            | boolean                        |           | not null | false
+     y_up                    | boolean                        |           | not null | false
+     digest                  | character varying              |           |          |
+     notes                   | text                           |           |          |
+     caption                 | text                           |           |          |
+     size                    | bigint                         |           |          |
+     presupported_version_id | bigint                         |           |          |
+    Indexes:
+        "model_files_pkey" PRIMARY KEY, btree (id)
+        "index_model_files_on_digest" btree (digest)
+        "index_model_files_on_filename_and_model_id" UNIQUE, btree (filename, model_id)
+        "index_model_files_on_model_id" btree (model_id)
+        "index_model_files_on_presupported_version_id" btree (presupported_version_id)
+    Foreign-key constraints:
+        "fk_rails_a411caf13d" FOREIGN KEY (model_id) REFERENCES models(id)
+        "fk_rails_b5ac05b6e3" FOREIGN KEY (presupported_version_id) REFERENCES model_files(id)
+    Referenced by:
+        TABLE "model_files" CONSTRAINT "fk_rails_b5ac05b6e3" FOREIGN KEY (presupported_version_id) REFERENCES model_files(id)
+    """
+
+    __tablename__ = "model_files"
+    id = Column(Integer, primary_key=True)
+    filename = Column(String)
+    model_id = Column(Integer, nullable=False)
+    created_at = Column(DateTime, nullable=False)
+    updated_at = Column(DateTime, nullable=False)
+    presupported = Column(Boolean, nullable=False)
+    y_up = Column(Boolean, nullable=False)
+    digest = Column(String)
+    notes = Column(Text)
+    caption = Column(Text)
+    size = Column(Integer)
+    presupported_version_id = Column(Integer)
+
+
 def sanitize_path(root_folder, path):
     return path.relative_to(root_folder)
 
 
+def insert_model_files(session, model_id, root_folder):
+    for path in root_folder.iterdir():
+        if path.is_file():
+            logger.info("Processing file %s", path.name)
+            model_file = ModelFile(
+                filename=path.name,
+                model_id=model_id,
+                created_at=datetime.now(),
+                updated_at=datetime.now(),
+                presupported=False,
+                y_up=False,
+                digest=calculate_digest(path),
+                notes="",
+                caption="",
+                size=path.stat().st_size,
+                presupported_version_id=None,
+            )
+            session.add(model_file)
+            session.commit()
+        elif path.is_dir():
+            logger.info("Processing directory %s", path.name)
+            insert_model_files(session, model_id, path)
+
+
+def calculate_digest(file):
+    """In JS it's Digest::SHA512.new.file(pathname).hexdigest"""
+    return hashlib.sha512(file.read_bytes()).hexdigest()
+
+
 # Function to recursively traverse the folder structure and insert data into the table
 def insert_folders(session, root_folder, library_id):
-    for creator in root_folder.iterdir():
-        for collection in creator.iterdir():
-            for model in collection.iterdir():
-                if model.is_dir():
-                    creator, collection, model_name, uuid = get_infos(model)
+    for creator_path in root_folder.iterdir():
+        logger.info("Processing creator %s", creator_path.name)
+        for collection_path in creator_path.iterdir():
+            logger.info("Processing collection %s", collection_path.name)
+            for model_path in collection_path.iterdir():
+                if not model_path.is_dir():
+                    continue
+                logger.info("Processing model %s", model_path.name)
 
-                    creator_id = get_or_create_creator(session, creator.name).id
-                    collection_id = get_or_create_collection(
-                        session, collection.name, None
-                    ).id
-                    model = Model(
-                        name=model.name,
-                        path=str(sanitize_path(root_folder, model)),
-                        library_id=library_id,
-                        created_at=datetime.now(),
-                        updated_at=datetime.now(),
-                        creator_id=creator_id,
-                        collection_id=collection_id,
-                    )
-                    session.add(model)
-                    session.commit()
+                creator, collection, model_name, uuid = get_infos(model_path)
+
+                creator_id = get_or_create_creator(session, creator.name).id
+                collection_id = get_or_create_collection(
+                    session, collection.name, None
+                ).id
+                model = Model(
+                    name=model_path.name,
+                    path=str(sanitize_path(root_folder, model_path)),
+                    library_id=library_id,
+                    created_at=datetime.now(),
+                    updated_at=datetime.now(),
+                    creator_id=creator_id,
+                    collection_id=collection_id,
+                )
+                session.add(model)
+                session.commit()
+                insert_model_files(session, model.id, model_path)
 
 
 def get_infos(path):
@@ -185,8 +278,11 @@ def main():
     parser.add_argument("psql_uri", type=str, help="The URI of the PostgreSQL database")
     args = parser.parse_args()
 
+    logging.basicConfig(level=logging.INFO)
+
     # Initialize the database connection
     session = init_engine(args.psql_uri)
+    session = MockSession()
 
     # Call the function to insert data into the table
     insert_folders(session, args.root_folder, args.library_id)
